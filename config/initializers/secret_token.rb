266b62edfba91838001b8dcb5e4709602b633b52
hhuai <xiao@bogon.(none)>
2010-12-15 14:01:21 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Synclive::Application.config.secret_token = '3ac0359d076f76505998acac2783ca969fe79241fefa993e3cc52f98b298e7411c5d77d0c4c670917a9cbfbe9720fcd8bc6ff68c4c7efc317f11ba26f0f7c19d'
