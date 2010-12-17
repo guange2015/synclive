5dcf08dee845068eff2cb2f591fbaacf65114c77
hhuai <8863824@gmail.com>
2010-12-17 13:12:37 +0800

    merger the old synclive project

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Synclive::Application.config.secret_token = '3ac0359d076f76505998acac2783ca969fe79241fefa993e3cc52f98b298e7411c5d77d0c4c670917a9cbfbe9720fcd8bc6ff68c4c7efc317f11ba26f0f7c19d'
+Synclive::Application.config.secret_token = '1a29e95115deaa215db6b78e541a5d02d5dba0a4b59c6013dc51bcf8a84c8f7ca5c00e8726c61f59a304a7a4ccc61469941de52c95366e04a12392bf39af4d28'
---
+++ db/migrate/20100919144119_create_contacts.rb
@@ -0,0 +1,15 @@
+class CreateContacts < ActiveRecord::Migration
+  def self.up
+    create_table :contacts do |t|
+      t.integer :user_id
+      t.string :name
+      t.string :tel
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :contacts
+  end
+end
