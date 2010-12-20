27496fab75459863d968835684f845dc5c0e4979
hhuai <8863824@gmail.com>
2010-12-20 16:54:42 +0800

    signinfo update

--- app/models/sign_info.rb
+++ app/models/sign_info.rb
@@ -1,5 +1,7 @@
 class SignInfo < ActiveRecord::Base
   belongs_to :user_info
+  validates_presence_of :user_info_id
+  validates_presence_of :content
 
   def self.modify_sign_info(user_id, content)
     if not UserInfo.exists? user_id
@@ -8,7 +10,8 @@ class SignInfo < ActiveRecord::Base
 
     update_all(:status=>0, :user_info_id => user_id)
 
-    user_info = UserInfo.new({:content => content, :status => 1})
-    user_info.save
+    sign_info = new({:user_info_id => user_id,:content => content, :status => 1})
+    return 0 if sign_info.save
+    return 2
   end
 end
